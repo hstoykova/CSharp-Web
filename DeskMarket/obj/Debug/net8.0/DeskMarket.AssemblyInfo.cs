470f774b18b4ad4b4ffaf75a38c6c53fc4a778ae
hstoykova <hristina.pencheva@gmail.com>
2025-02-24 10:20:26 +0200

    some small changes

--- DeskMarket/obj/Debug/net8.0/DeskMarket.AssemblyInfo.cs
+++ DeskMarket/obj/Debug/net8.0/DeskMarket.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DeskMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ac5228616c1fb4f4371ab5a305751d263e3eef4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9536be726299329821259410beb8033675825364")]
 [assembly: System.Reflection.AssemblyProductAttribute("DeskMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DeskMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
